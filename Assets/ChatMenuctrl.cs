91b1b0fcfae2ef27bdf9cff71efc0bbf478a7be1
Kyle <clapp41@yahoo.com>
2017-04-19 20:33:34 -0400

    started chat menu

---
+++ Assets/ChatMenuctrl.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+public class ChatMenuctrl : MonoBehaviour 
+{
+	public void LoadStringTable(string sceneName)
+	{
+		SceneManager.LoadScene (sceneName);
+	}
+
+}
