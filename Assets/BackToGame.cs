f19ca6de7890fd758f134dd0fb8050d8b08961f3
Kyle <clapp41@yahoo.com>
2017-04-20 22:04:57 -0400

    more stuff

---
+++ Assets/BackToGame.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+public class BackToGame : MonoBehaviour {
+	public void BackToScene(string sceneName)
+	{
+		SceneManager.LoadScene (sceneName);
+	}
+}
---
+++ Assets/stringTable.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+public class stringTable : MonoBehaviour {
+	public ChatMenuctrl chatMenu;
+	public void FillStringTable(string sceneName)
+	{
+		
+		SceneManager.LoadScene (sceneName);
+	}
+}
